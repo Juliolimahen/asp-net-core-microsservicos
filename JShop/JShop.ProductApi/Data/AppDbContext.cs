a5049a176beb26f73545fccfc0d9c21677b99ced
Juliolimahen <juliolima_henrique@outlook.com>
2022-07-18 23:20:24 -0300

    Create API

---
+++ JShop/JShop.ProductApi/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JShop.ProductApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JShop.ProductApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        /// <summary>
+        /// Mapear classe Category para a tabela Categories
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+
+        /// <summary>
+        /// Mapear classe Product para a tabela Products
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ JShop/JShop.ProductApi/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace JShop.ProductApi.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Define que eu tenha uma lista de produtos para uma categoria
+    /// </summary>
+    public ICollection<Product> Products { get; set; }
+}
