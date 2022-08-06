b7409c99c27ade7e1f4b2ea731aafdcc8b3da47b
Juliolimahen <juliolima_henrique@outlook.com>
2022-08-06 17:29:56 -0300

    fix Json ignore

--- JShop/JShop.ProductApi/Dtos/ProductDto.cs
+++ JShop/JShop.ProductApi/Dtos/ProductDto.cs
@@ -1,5 +1,6 @@
 ﻿using JShop.ProductApi.Models;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace JShop.ProductApi.Dtos
 {
@@ -12,19 +13,20 @@ namespace JShop.ProductApi.Dtos
         [MaxLength(100)]
         public string Name { get; set; }
 
-        [Required(ErrorMessage ="The Price is Required")]
+        [Required(ErrorMessage = "The Price is Required")]
         public decimal Price { get; set; }
 
-        [Required(ErrorMessage ="The Description is Required")]
+        [Required(ErrorMessage = "The Description is Required")]
         [MinLength(5)]
         [MaxLength(200)]
         public string? Description { get; set; }
 
-        [Required(ErrorMessage ="The Stock is Required")]
-        [Range(1,9999)]
+        [Required(ErrorMessage = "The Stock is Required")]
+        [Range(1, 9999)]
         public long Stock { get; set; }
         public string? ImageURL { get; set; }
 
+        [JsonIgnore]
         public Category? Category { get; set; }
         public int CategoryId { get; set; }
     }
--- JShop/JShop.ProductApi/Program.cs
+++ JShop/JShop.ProductApi/Program.cs
@@ -2,12 +2,13 @@ using JShop.ProductApi.Data;
 using JShop.ProductApi.Repositories;
 using JShop.ProductApi.Services;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(x=>x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
